1bb3615b34882ebce68660e4d55a8f2e0534cdc0
michele <m.michelemarzioni@gmail.com>
2026-08-30 15:46:03 +0200

    feat: enhance authSchema validation by adding trimming and maximum length constraints for username and password

--- src/validation/user.validation.ts
+++ src/validation/user.validation.ts
@@ -1,8 +1,14 @@
 import * as z from "zod";
 
 export const authSchema = z.object({
-  username: z.string("username is mandatory").min(3, "Username must be at least 3 characters long"),
-  password: z.string("password is mandatory").min(6, "Password must be at least 6 characters long"),
+  username: z.string("username is mandatory")
+    .trim()
+    .min(3, "Username must be at least 3 characters long")
+    .max(100),
+  password: z.string("password is mandatory")
+    .trim()
+    .min(6, "Password must be at least 6 characters long")
+    .max(100),
 });
 
 
